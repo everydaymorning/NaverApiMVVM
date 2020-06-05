6040e95433ca4ff0f8358bb28542aa0628de308b
everydaymorning <zmflrqll@naver.com>
2020-06-05 15:13:11 +0900

    #1 okhttp, naver login gradle 추가 및 naver api id, secret 초기화

---
+++ RefactoringProject/app/src/main/java/com/example/refactoringproject/network/RetrofitNetwork.kt
@@ -0,0 +1,12 @@
+package com.example.refactoringproject.network
+
+import com.example.refactoringproject.R
+
+interface RetrofitNetwork {
+
+
+    companion object{
+        private const val mClientId = R.string.CLIENT_ID
+        private const val mClientSecret = R.string.CLIENT_SECRET
+    }
+}
