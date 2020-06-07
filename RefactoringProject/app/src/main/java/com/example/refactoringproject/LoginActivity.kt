b54382c13d08034b5c66e6b410b7802f93dfa154
everydaymorning <zmflrqll@naver.com>
2020-06-07 17:15:44 +0900

    #3 네이버 아이디 로그인 api 오류 발생

--- RefactoringProject/app/src/main/java/com/example/refactoringproject/LoginActivity.kt
+++ RefactoringProject/app/src/main/java/com/example/refactoringproject/LoginActivity.kt
@@ -30,34 +30,10 @@ class LoginActivity : AppCompatActivity() {
             getString(R.string.CLIENT_NAME)
         )
 
-        btn_naver_button.setOAuthLoginHandler(@SuppressLint("HandlerLeak")
-        object: OAuthLoginHandler(){
-            override fun run(success: Boolean){
-                if(success){
-                    val accessToken = oAuthLoginModule.getAccessToken(mContext)
-                    val refreshToken = oAuthLoginModule.getRefreshToken(mContext)
-                    val expiresAt = oAuthLoginModule.getExpiresAt(mContext)
-                    val tokenType = oAuthLoginModule.getTokenType(mContext)
-                    Log.i("TAG", "nhn Login Access Token : $accessToken")
-                    Log.i("TAG", "nhn Login refresh Token : $refreshToken")
-                    Log.i("TAG", "nhn Login expiresAt : $expiresAt")
-                    Log.i("TAG", "nhn Login Token Type : $tokenType")
-                    Log.i("TAG", "nhn Login Module State : " + oAuthLoginModule.getState(mContext).toString())
-                    val intent = Intent(mContext, MainActivity::class.java)
-                    startActivity(intent)
-
-                } else{
-                    val errorCode = oAuthLoginModule.getLastErrorCode(mContext)
-                    val errorDesc = oAuthLoginModule.getLastErrorDesc(mContext)
-                    Toast.makeText(mContext, "error code: $errorCode  errorDesc: $errorDesc", Toast.LENGTH_LONG).show()
-                }
-            }
-        })
-
+        btn_naver_button.setOnClickListener{
+            naverSignIn()
+        }
     }
-
-
-
 
     private fun naverSignIn(){
         if(oAuthLoginModule.getState(this) == OAuthLoginState.OK){
@@ -65,12 +41,45 @@ class LoginActivity : AppCompatActivity() {
                 val url = "https://openapi.naver.com/v1/nid/me"
                 val at = oAuthLoginModule.getAccessToken(mContext)
                 val result = oAuthLoginModule.requestApi(mContext, at, url)
+                val startToken = "<message>"
+                val endToken = "</message>"
+                val startIndex = result?.indexOf(startToken) ?: -1
+                val endIndex = result?.indexOf(endToken) ?: -1
+                if (startIndex == -1 || endIndex <= 0){
 
+                }
+                val message = result?.substring(startIndex + startToken.length, endIndex)?.trim()
+                if(message == ("success")){ Log.d("TAG", "Success RequestNHNLoginApiTask")
+                    val intent = Intent(mContext, MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Log.e("TAG", "Login Fail")
+                }
             }
+        }else{
+            oAuthLoginModule.startOauthLoginActivity(this, @SuppressLint("HandlerLeak")
+            object : OAuthLoginHandler(){
+                override fun run(success: Boolean){
+                    if(success){
+                        val accessToken = oAuthLoginModule.getAccessToken(mContext)
+                        val refreshToken = oAuthLoginModule.getRefreshToken(mContext)
+                        val expiresAt = oAuthLoginModule.getExpiresAt(mContext)
+                        val tokenType = oAuthLoginModule.getTokenType(mContext)
+                        Log.i("TAG", "nhn Login Access Token : $accessToken")
+                        Log.i("TAG", "nhn Login refresh Token : $refreshToken")
+                        Log.i("TAG", "nhn Login expiresAt : $expiresAt")
+                        Log.i("TAG", "nhn Login Token Type : $tokenType")
+                        Log.i("TAG", "nhn Login Module State : " + oAuthLoginModule.getState(mContext).toString())
+                        val intent = Intent(mContext, MainActivity::class.java)
+                        startActivity(intent)
+
+                    } else{
+                        val errorCode = oAuthLoginModule.getLastErrorCode(mContext)
+                        val errorDesc = oAuthLoginModule.getLastErrorDesc(mContext)
+                        Toast.makeText(mContext, "error code: $errorCode  errorDesc: $errorDesc", Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
         }
     }
-
-
-
-
 }
